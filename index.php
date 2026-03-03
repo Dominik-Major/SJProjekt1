7834c220f1a1996acebda2ecfd019aeed1c04f06
Dominik-Major <major.domiiniik@gmail.com>
2026-03-03 15:08:13 +0100

    Prvy Commit
    Zmena v strukture kodu

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php echo "<h1>Hello World</h1>" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP Cviko</h1>
+    <br>
+    <?php
+    echo "<p>Hello World</p>\n";
+    echo "date : ". date('l jS \of F Y h:i:s A');
+    ?>
+</body>
+</html>
